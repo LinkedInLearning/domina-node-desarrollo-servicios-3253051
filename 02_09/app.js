33bf7f3039f28ec9462a8a0266aba46f58fd0c1b
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-13 18:25:45 +0100

    02_09 Error de indefinido el req.body en Express.js

---
+++ 02_09/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/usuario', (request, response) => {
+  const nombre = request.body.nombre;
+  const email = request.body.email;
+
+  response.send(`Datos recibidos con éxito:\n
+          Nombre: ${nombre} \n
+          Email: ${email}`);
+});
+
+app.listen(3000, () => {
+  console.log('La aplicación está corriendo en http://localhost:3000');
+});
